52cd4aad8bd36854852055da2de836b85329ff87
David Albornoz <albornozd@usf.edu>
2026-02-05 19:44:35 -0500

    created customer records in js

---
+++ cc_3.js
@@ -0,0 +1,47 @@
+let customers = [
+    {
+        customerName: "Alex Taylor",
+        email: "alextaylor@usf.edu",
+        purchases: [
+            "Blue Shirt",
+            "Black Socks",
+            "Brown Belt"
+        ]
+    },
+    {
+        customerName: "Jordan Smith",
+        email: "jordansmith@usf.edu",
+        purchases: [
+            "White Shirt",
+            "Blue Jeans",
+            "Grey Jacket"
+        ]
+    },
+    {
+        customerName: "Casey Miller",
+        email: "caseymiller@usf.edu",
+        purchases: [
+            "Black Shirt",
+            "White Socks",
+            "Black Shoes"
+        ]
+    },
+    {
+        customerName: "Morgan Reed",
+        email: "morganreed@usf.edu",
+        purchases: [
+            "Gray Shirt",
+            "Khaki Pants",
+            "Baseball Cap"
+        ]
+    },
+    {
+        customerName: "Jamie Clark",
+        email: "jamieclark@usf.edu",
+        purchases: [
+            "Navy Shirt",
+            "Brown Wallet",
+            "Red Heels"
+        ]
+    },
+]
